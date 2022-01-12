504686758602a094ea5c448fab0f8c6786e714eb
Michael Brandt <msbrandt00@gmail.com>
2022-01-12 09:07:55 -0700

    read in age and salary from hash as  integers

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,8 +1,8 @@
 class Employee
-  attr_reader :name, :age, :salary 
+  attr_reader :name, :age, :salary
   def initialize(hash)
     @name = hash[:name]
-    @age = hash[:age]
-    @salary = hash[:salary]
+    @age = hash[:age].to_i
+    @salary = hash[:salary].to_i
   end
 end
